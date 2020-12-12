2d05d23f0aed9bc780e4af0d2e0b9a5b3741ebc4
Shubham Pratap Singh <SPSingh@bpl.netlink.com>
2020-12-12 14:18:57 +0530

    changes

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const arr = ['Hey there, I am SPSingh', 'Hey there, I am Aman']
+  const [arrPos, setarrPos] = useState(0)
+  const [title, settitle] = useState(arr[arrPos])
+  const changeTitle = () => {
+    if (arrPos == 0) {
+      setarrPos(1)
+      settitle(arr[1])
+    }
+    else {
+      setarrPos(0)
+      settitle(arr[0])
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{title}</Text>
+      <Button onPress={changeTitle} title="Switch person" />
       <StatusBar style="auto" />
     </View>
   );
